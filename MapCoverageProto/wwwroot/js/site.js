c9a4f2a9d283cd5e21af75a19c87b95cfd107ef1
c4meronMcc <122793215+c4meronMcc@users.noreply.github.com>
2023-05-25 09:34:54 +0100

    w

---
+++ MapCoverageProto/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// When the user scrolls the page, execute myFunction
+$(window).on('scroll', function () {
+
+    var windowTop = $(window).scrollTop();
+    var elementTop = $('.anchor').offset().top;
+
+    if (windowTop > elementTop) {
+        $(".sidebar").addClass("fixed");
+        $(".header").addClass("fixed");
+    } else {
+        $(".sidebar").removeClass("fixed");
+        $(".header").removeClass("fixed");
+    }
+
+});
